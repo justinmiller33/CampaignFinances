7f1e6f178c051e5df84c158064cfed46c87e22be
justinmiller33 <48934159+justinmiller33@users.noreply.github.com>
2020-04-14 13:43:26 -0400

    Working with DataFrames
    Next time, look to allocate max/min y values and slope in seperate arrays for easier transition to polygon search algorithm

--- legFinder.py
+++ legFinder.py
@@ -1,38 +1,83 @@
 import shapefile
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
+#Reading Data from .shp file
 sf = shapefile.Reader("/Users/justi/OneDrive/Documents/datasets/OCPF Data/senate2012/SENATE2012_POLY.shp")
 
-for j in range(40):
-    feature = sf.shapeRecords()[j]
-    first = feature.shape.__geo_interface__
 
-    data = np.asarray(first["coordinates"])
+def lengthFinder(numDistricts):
+    maxLengths = np.zeros(numDistricts)
+    for j in range(numDistricts):
+        feature = sf.shapeRecords()[j]
+        first = feature.shape.__geo_interface__
 
-    x = np.array([])
-    y = np.array([])
+        data = np.asarray(first["coordinates"])
 
-    #Don't need to identify specific values, just use exception handling
-    #if j>0 and j<7 or j>14 and j<17 or j==18:
-    try:
-        for i in range (np.size(data[0][0], axis=0)):
-            x = np.append([x],data[0][0][i][0])
-            y = np.append([y],data[0][0][i][1])
+        x = np.array([])
+        y = np.array([])
 
-    except:
+        #Don't need to identify specific values, just use exception handling
+        #if j>0 and j<7 or j>14 and j<17 or j==18:
         try:
-    #elif j<13: #and j!=:
-            for i in range (np.size(data, axis=1)):
-                x = np.append([x],data[:,i][0][0])
-                y = np.append([y],data[:,i][0][1])
-        except:
-    #else:
-            for i in range (np.size(data[0], axis=0)):
-                x = np.append([x],data[0][i][0])
-                y = np.append([y],data[0][i][1])
-        
-    plt.plot(x,y)
-    
-plt.show()
+            for i in range (np.size(data[0][0], axis=0)):
+                x = np.append([x],data[0][0][i][0])
+                y = np.append([y],data[0][0][i][1])
 
+        except:
+            try:
+        #elif j<13: #and j!=:
+                for i in range (np.size(data, axis=1)):
+                    x = np.append([x],data[:,i][0][0])
+                    y = np.append([y],data[:,i][0][1])
+            except:
+        #else:
+                for i in range (np.size(data[0], axis=0)):
+                    x = np.append([x],data[0][i][0])
+                    y = np.append([y],data[0][i][1])
+
+        maxLengths[j] = len(x)
+        
+    return(maxLengths)
+
+def dataExtract(numDistricts, maxLength):
+    maxLengths = np.zeros(numDistricts)
+    for j in range(numDistricts):
+        feature = sf.shapeRecords()[j]
+        first = feature.shape.__geo_interface__
+
+        data = np.asarray(first["coordinates"])
+
+        df = np.zeros([2,numDistricts,maxLength])
+        
+
+        #Don't need to identify specific values, just use exception handling
+        #if j>0 and j<7 or j>14 and j<17 or j==18:
+        try:
+            for i in range (np.size(data[0][0], axis=0)):
+                x = np.append([x],data[0][0][i][0])
+                y = np.append([y],data[0][0][i][1])
+
+        except:
+            try:
+        #elif j<13: #and j!=:
+                for i in range (np.size(data, axis=1)):
+                    x = np.append([x],data[:,i][0][0])
+                    y = np.append([y],data[:,i][0][1])
+            except:
+        #else:
+                for i in range (np.size(data[0], axis=0)):
+                    x = np.append([x],data[0][i][0])
+                    y = np.append([y],data[0][i][1])
+
+        for k in range(len(x)):
+            df[0,j,k]=x[k]
+            df[1,j,k]=y[k]
+
+    return df
+
+#_____________________________________________________________
+        
+lengths = lengthFinder(40)
+data = dataExtract(40,int(np.max(lengths)))
