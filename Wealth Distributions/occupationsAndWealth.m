0ed8a1bac34453128083ba2acd96fcc468267906
justinmiller33 <48934159+justinmiller33@users.noreply.github.com>
2020-05-11 15:17:08 -0400

    Comparing Occupations and Home Prices
    Validating the use of Occupations as indicators of wealth by using Feeney's donor's home prices.

--- Wealth Distributions/occupationsAndWealth.m
+++ Wealth Distributions/occupationsAndWealth.m
@@ -1,21 +1,24 @@
 %Looking at occupations
-%58.27 percent of donations list occupations too
+%58.27 percent of donorData list occupations too
 %% Gathering Occupation Groupings
 
 load("feeneyWithHomes.mat")
 
+donorData = FeeneyWithHomes;
+
 jobs = [];
-for i=1:height(donations)
-    jobs = [jobs;donations(i,:).Occupation];
+for i=1:height(donorData)
+    jobs = [jobs;donorData(i,:).Occupation];
 end
 
 usJobs = jobs;
-lengthListed = length(donations.Amount(usJobs~="" & donations.RecordTypeDescription == "Individual"));
+lengthListed = length(donorData.Amount(usJobs~="" & donorData.RecordTypeDescription == "Individual"));
 
-empty = (jobs =="");
+empty = ismissing(jobs);
+
 jobs(empty) = [];
 
-jobs = strip(jobs);
+jobs = strip(string(jobs));
 
 keywords = [];
 for i = 1:length(jobs)
@@ -38,28 +41,56 @@ data = table(wordsByFrequency,rankOfOccurrences,wordProportion);
 means = zeros(height(data),1);
 stds = zeros(height(data),1);
 for i=1:height(data)
-    jobIndex = find(contains(lower(usJobs),wordsByFrequency(i)) & donations.RecordTypeDescription=="Individual");
-    jobMean = mean(donations.Amount(jobIndex));
-    jobStd = std(donations.Amount(jobIndex));
+    jobIndex = find(contains(lower(string(usJobs)),wordsByFrequency(i)) & donorData.RecordTypeDescription=="Individual");
+    jobMean = mean(donorData.Amount(jobIndex));
+    jobStd = std(donorData.Amount(jobIndex));
     
     means(i) = jobMean;
     stds(i) = jobStd;
 end
 sums = rankOfOccurrences.*means;
-totalProportion = sums/sum(donations.Amount);
+totalProportion = sums/sum(donorData.Amount);
 impactRatio = totalProportion./wordProportion;
 
 data = [data, table(means,stds,sums,totalProportion)];
 
-%% Grouping for those with at least 20 values (arbitrary)
+%% Grouping for those with at least [threshold] values (arbitrary)
+threshold = 5
 
-commonIndices = find(data.rankOfOccurrences>=20);
+commonIndices = find(data.rankOfOccurrences>=threshold);
 
-[meanDonations, meanInd] = sort(data.means(commonIndices),'Descend');
+[meanDonorData, meanInd] = sort(data.means(commonIndices),'Descend');
 
 orderedJobs = data.wordsByFrequency(commonIndices);
 orderedJobs = orderedJobs(meanInd);
 
 %plot range grouped for means
-bar(meanDonations);
+bar(meanDonorData);
 %xticklabels(orderedJobs(end-10:end));
+
+%% For Feeney,only. Matching with home estimates
+allMeans = [];
+
+for j=1:length(find(data.rankOfOccurrences>threshold))
+    
+targetJob = data(j,:).wordsByFrequency;
+homeMeans = [];
+for i=1:height(FeeneyWithHomes)
+    if contains(lower(string(FeeneyWithHomes(i,:).Occupation)),targetJob)
+        if FeeneyWithHomes(i,:).zestimate ~= 0 & FeeneyWithHomes(i,:).RecordTypeDescription == "Individual"
+            homeMeans = [homeMeans, FeeneyWithHomes(i,:).zestimate];
+        end
+    end
+end
+allMeans = [allMeans; mean(homeMeans)];
+end
+%% 
+
+[sortedMeans, homeSortIdx] = sort(allMeans,'descend');
+jobsByHomePrice = data(1:j,:).wordsByFrequency(homeSortIdx);
+
+homeTables = table(sortedMeans,jobsByHomePrice);
+bar(sortedMeans)
+hold on
+xticklabels(jobsByHomePrice)
+
