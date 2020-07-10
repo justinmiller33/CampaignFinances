30ca148f6358cc5b48dff72c3b37034cc319c0dd
justinmiller33 <48934159+justinmiller33@users.noreply.github.com>
2020-07-10 17:13:04 -0400

    BISG

--- District Locating/legMapper.py
+++ District Locating/legMapper.py
@@ -1,38 +1,45 @@
 import shapefile
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 sf = shapefile.Reader("C:\devel\CampaignFinances\Locater\senate2012\SENATE2012_POLY.shp")
 
-for j in range(40):
-    feature = sf.shapeRecords()[j]
-    first = feature.shape.__geo_interface__
+for k in range(1,41):
+    for j in range(k):
+        feature = sf.shapeRecords()[j]
+        first = feature.shape.__geo_interface__
 
-    data = np.asarray(first["coordinates"])
+        data = np.asarray(first["coordinates"])
 
-    x = np.array([])
-    y = np.array([])
+        x = np.array([])
+        y = np.array([])
 
-    #Don't need to identify specific values, just use exception handling
-    #if j>0 and j<7 or j>14 and j<17 or j==18:
-    try:
-        for i in range (np.size(data[0][0], axis=0)):
-            x = np.append([x],data[0][0][i][0])
-            y = np.append([y],data[0][0][i][1])
-
-    except:
+        #Don't need to identify specific values, just use exception handling
+        #if j>0 and j<7 or j>14 and j<17 or j==18:
         try:
-    #elif j<13: #and j!=:
-            for i in range (np.size(data, axis=1)):
-                x = np.append([x],data[:,i][0][0])
-                y = np.append([y],data[:,i][0][1])
+            for i in range (np.size(data[0][0], axis=0)):
+                x = np.append([x],data[0][0][i][0])
+                y = np.append([y],data[0][0][i][1])
+
         except:
-    #else:
-            for i in range (np.size(data[0], axis=0)):
-                x = np.append([x],data[0][i][0])
-                y = np.append([y],data[0][i][1])
+            try:
+        #elif j<13: #and j!=:
+                for i in range (np.size(data, axis=1)):
+                    x = np.append([x],data[:,i][0][0])
+                    y = np.append([y],data[:,i][0][1])
+            except:
+        #else:
+                for i in range (np.size(data[0], axis=0)):
+                    x = np.append([x],data[0][i][0])
+                    y = np.append([y],data[0][i][1])
+
         
-    plt.plot(x,y)
+        plt.plot(x,y)
+
+    if k%5 == 0:
+        plt.show()
     
-plt.show()
+    
+
 
---
+++ Race/BISG.py
@@ -0,0 +1,104 @@
+# BISG
+import numpy as np
+import pandas as pd
+import wikipedia
+import re
+
+# Load in demographic data
+# http://archive.boston.com/news/local/massachusetts/graphics/03_22_11_2010_census_town_population/#
+dem = pd.read_excel('demographics.xlsx')
+
+# Here's our mass averages too for towns w/ missing data
+# https://www.census.gov/quickfacts/MA
+# Same format, [white, whiteProp, black, blackProp, asian, asianProp, hispanic, hispanicProp]
+massPop = 6547629 
+mass = np.array([massPop * 0.711, 0.711, massPop * 0.09, 0.09, massPop * .072, .072, massPop * 0.124, 0.124])
+
+# Here's our US Counts for Bayes Rule
+usPop = 308758105
+us = np.array([usPop * 0.601, 0.601, usPop * 0.134, 0.134, usPop * 0.059, 0.059, usPop * 0.185, 0.185])
+
+# Load straight surname proportions by loading the completed xlsx (house or senate)
+pathToLocater = 'C:/devel/CampaignFinances/Locater/'
+data = pd.read_excel(pathToLocater + "masshousefullformatted.xlsx")
+
+# Getting general surname probs (Just for the 4 Detectables this time)
+whiteGen = data.White
+asianGen = data.Asian
+blackGen = data.Black
+hispanicGen = data.Hispanic
+
+# Function to find index of current city in demographic sheet
+findCityIndex = lambda subject : np.where(data.City[subject] == dem.Town)[0][0]
+
+# Function to get race values of percentage of US Population of that race
+getRaceProps = lambda dem,cityIndex : np.array([dem.White[cityIndex] / us[0], dem.Black[cityIndex] / us[4], dem.Asian[cityIndex] / us[2], dem.Hispanic[cityIndex] / us[6]])
+
+# Getting massachusetts race props
+maRaceProps = np.array([mass[0] / us[0], mass[4] / us[4], mass[2] / us[2], mass[6] / us[6]])
+
+# Defaulting cityRaces to maRaceProps
+cityRaces = maRaceProps
+
+# Output array of BISG probabilities
+output = np.zeros((len(data),4))
+                  
+# Subject is the row number in the overall spreadsheet
+def bayes(subject):
+    # Globalizing
+    global whiteGen
+    global asianGen
+    global blackGen
+    global hispanicGen
+    global data
+    global dem
+
+    global mass
+    global maRaceProps
+    global us
+    global cityRaces
+
+    global output
+
+    oldProps = np.array([data.White[subject], data.Asian[subject], data.Black[subject], data.Hispanic[subject]])
+
+    # Check for logic
+    # print(oldProps)
+    # print(data.City[subject])    
+
+    # If out of mass, I'm not gonna bother to do BISG. Just keep the same probs.
+    if data.State[subject] != 'MA':
+        # Save subjects output
+        output[subject] = np.array([data.White[subject], data.Asian[subject], data.Black[subject], data.Hispanic[subject]])
+
+    else:
+    # Find the index of city
+    # If not in spreadsheet, then keep MA stats as your output (better than nothing)
+        try:
+            # Get index of props
+            cityIndex = findCityIndex(subject)
+            # Get raceprops for that city
+            cityRaces = getRaceProps(dem,cityIndex)
+
+        # Will except if city not in list of cities
+        # Use MA demographics
+        except:
+            cityRaces = maRaceProps
+
+        # For each race: in order white, asian, black, hispanic
+        for i in range(4):
+                  
+            # BISG Formula
+            output[subject,i] =  (cityRaces[i] * oldProps[i]) / sum(cityRaces * oldProps)
+
+    # More checks
+    # print(cityRaces)
+    # return output[subject]
+
+
+# Running Bayes for all subjects
+for subject in range(len(data)):
+    bayes(subject)
+    if subject%1000 == 500:
+        print(subject/len(data))
+                
