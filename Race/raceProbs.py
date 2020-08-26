f9630d523a14cf51a8cb454356b181ae1a87e34a
Justin Miller <48934159+justinmiller33@users.noreply.github.com>
2020-08-26 11:52:44 -0400

    Final

--- Race/raceProbs.py
+++ Race/raceProbs.py
@@ -1,32 +1,33 @@
-#Scraping census api to get probability of race by names
-#Using Central Limit Thm to calculate features of contributions by race
+# Scraping census api to get probability of race by names
+# Using Central Limit Thm to calculate features of contributions by race
 
 import numpy as np
 import pandas as pd
 import requests
 import time
 
-#Loading in names
+# Loading in names
 start = time.time()
 names = pd.read_excel("C:\devel\CampaignFinances\Locater\house_full_individual.xlsx")
 names = names.to_numpy()
 print("TIME TO LOAD: "+str(time.time()-start))
 
-#Location of donor names in spreadsheet
+# Location of donor names in spreadsheet
 nl = 9
 
+# Function to get all unique surnames from dataset
 def getName(names,nl,n):
     surnames = {}
 
     for i in range(n):
-        #Gettting your current name (w/ exception handler)
+        # Gettting your current name (w/ exception handler)
         try:
             current = names[i,nl].split(",")[0].lower()
 
         except:
             continue
         
-        #Checking to make sure its not an organization
+        # Checking to make sure its not an organization
         if " " not in current:
             
             if current not in surnames:
@@ -36,13 +37,14 @@ def getName(names,nl,n):
                 surnames[current] = np.append(surnames[current],[i])
             
     return surnames
-        
+
+# Starting Run
 start = time.time()
 surnames = getName(names,nl,len(names))
 print("Surnames calculated in "+str(time.time()-start))
 print(str(len(surnames))+" surnames gathered from "+str(len(names))+" donors")
 
-
+# Function to get racial proportions for each unique surname
 def getProps(name):
 
     #Enter key since >500 iterations
@@ -54,16 +56,21 @@ def getProps(name):
     r = response.json()[1][0:4]
     return r
 
-
+# Starting loop through surnames
 props = np.zeros((len(surnames.keys()),4))
 loopStart = time.time()
+
+# For each surname
 for i in range(len(surnames.keys())):
+    
     #Saving and updating every 100 names
     if i%100 == 99:
+    
         np.save('racePropsHouseNewer.npy',props)
         print(str((time.time()-loopStart)/i)+" seconds per loop")
         print(str(((len(surnames.keys())-i)*(time.time()-loopStart)/i)/60)+" minutes remaining")
-    
+        
+    # Try to get proportions
     try:
         currentProps = getProps(list(surnames.keys())[i])
 
@@ -78,11 +85,17 @@ for i in range(len(surnames.keys())):
     except:
          continue
 
-
+# Function to map unique surnames to all surnames in dataset
 def normalize(names,nl,surnames,props):
+
     realProps = np.zeros((len(names),4))
+
+    # For each surname
     for i in range(len(surnames)):
+        # Find contributions with that surname
         spots = surnames[list(surnames.keys())[i]]
+        
+        # Declare that contributor's race proportions
         for j in range(len(spots)):
             realProps[spots[j]] = props[i]
 
@@ -91,8 +104,7 @@ def normalize(names,nl,surnames,props):
 
     return realProps
         
-    
-    
+# Saving
 realProps = normalize(names,nl,surnames,props)
 np.save('realPropsHouseNew.npy',realProps)
     
